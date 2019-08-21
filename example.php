7d1b2d3cd51baa38619ce2af2f0114eb783d497a
Alejo Sotelo <alejoasotelo@outlook.com>
2019-08-21 17:00:20 -0300

    [+] Agregué un ejemplo de cómo usar la api.

---
+++ example.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/andreani.php';
+
+use AlejoASotelo\Andreani;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$user = 'miuser';
+$pass = 'mipass';
+$cliente = 'CL9999999';
+$debug = true;
+
+$ws = new Andreani($user, $pass, $cliente, $debug);
+$result = $ws->getProvincias();
+
+var_dump($result);
