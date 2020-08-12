b0303e56f094635223521bbd29d0938b85c942f4
Alejo Sotelo <alejoasotelo@outlook.com>
2020-08-12 14:13:21 -0300

    [+] Agregué ejemplos de getOrden y getEnvio.

---
+++ examples/getEnvio.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once dirname(__DIR__).'/vendor/autoload.php';
+
+use AlejoASotelo\Andreani;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$user = 'miuser';
+$pass = 'mipass';
+$cliente = 'CL9999999';
+$debug = true;
+
+$ws = new Andreani($user, $pass, $cliente, $debug);
+
+$numeroDeEnvio = '360000001111111';
+
+$response = $ws->getEnvio($numeroDeEnvio);
+
+var_dump($response);
+
+if (!is_null($response)) {
+    file_put_contents(__DIR__.'/getEnvio.json', json_encode($response));
+}
