c60a52cf9b4a839ade05e7a236964ebbba1a9aab
Alejo Sotelo <alejoasotelo@outlook.com>
2020-09-21 11:30:56 -0300

    [*] Ahora es necesario cambiar el nombre de config.json.dist a config.json y configurar las credenciales para hacer las pruebas.

--- examples/config.php
+++ examples/config.php
@@ -8,6 +8,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+define('FILEPATH_CONFIG', __DIR__ .'/config.json');
+
+if (!file_exists(FILEPATH_CONFIG)) {
+    die('Falta el archivo config.json. Renombre el archivo config.json.dist a config.json e ingrese sus crendeciales de Andreani.');
+}
+
 $config = new Registry;
-$config->loadFile(__DIR__ . '/config.json', 'json');
+$config->loadFile(FILEPATH_CONFIG, 'json');
 
