f36ce07774c0a32857568e8ad619412383818748
Alejo Sotelo <alejoasotelo@outlook.com>
2020-09-21 11:32:36 -0300

    [*] cambié el texto de la alerta respecto a config.json.dist

--- examples/config.php
+++ examples/config.php
@@ -11,7 +11,7 @@ error_reporting(E_ALL);
 define('FILEPATH_CONFIG', __DIR__ .'/config.json');
 
 if (!file_exists(FILEPATH_CONFIG)) {
-    die('Falta el archivo config.json. Renombre el archivo config.json.dist a config.json e ingrese sus crendeciales de Andreani.');
+    die('(!) Falta el archivo config.json. Renombre el archivo config.json.dist a config.json e ingrese sus crendeciales de Andreani.');
 }
 
 $config = new Registry;
