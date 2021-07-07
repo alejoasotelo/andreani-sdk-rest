e597c465d0c631e831c50b2f7471ded8b378be2e
alejoasotelo <alesotelo@gmail.com>
2021-07-07 20:04:46 -0300

    [+] Agregué la función getVersion para saber la versión de la librería

--- src/Andreani.php
+++ src/Andreani.php
@@ -52,6 +52,7 @@ class Andreani
         $this->http = new Http($options, $transport);
     }
 
+    public function getVersion() {
     private function getBaseUrl($endpoint = 'login')
     {
         $base = $this->debug ? self::BASE_URL_DEV : self::BASE_URL_PROD;
