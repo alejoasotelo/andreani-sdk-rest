010d8d242651e13893d53bc4d36837f845607cbb
Alejo Sotelo <alejoasotelo@outlook.com>
2021-07-15 12:01:15 -0300

    [*] Actualicé el ejemplo de getSucursales.

--- examples/getSucursales.php
+++ examples/getSucursales.php
@@ -11,33 +11,8 @@ $debug = $config->get('debug', true);
 
 $ws = new Andreani($user, $pass, $cliente, false);
 
-$result = $ws->getSucursales(['canal' => 'B2C']);
+$response = $ws->getSucursales(['canal' => 'B2C']);
 
-$sucursales = [];
-foreach ($result as $sucursal) {
-
-    $dir = $sucursal->direccion;
-
-    if (!empty($dir->provincia) && !empty($dir->localidad) && !empty($dir->calle)) {
-
-        $row = [
-            'id_sucursal' => '"'.$sucursal->numero.'"',
-            'provincia' => '"'.addslashes($dir->provincia).'"',
-            'ciudad' => '"'.addslashes($dir->localidad).'"',
-            'direccion' => '"'.addslashes($dir->calle.' '.$dir->numero.', '.$dir->codigoPostal.', '.$dir->localidad.', '.$dir->provincia).'"',
-            'descripcion' => '"'.addslashes($sucursal->descripcion).'"',
-            'hora_de_trabajo' => '"'.addslashes($sucursal->horarioDeAtencion).'"',
-            'atencion_al_cliente' => $sucursal->datosAdicionales->seHaceAtencionAlCliente ? 1 : 0
-        ];
-    
-        $sucursales[] = '('.implode(',', $row).')';
-
-    }
-
+if (!is_null($response)) {
+    file_put_contents(__DIR__.'/getSucursales.json', json_encode($response));
 }
-
-$sql = 'INSERT INTO #__saandreani_sucursales_v2 
-    (id_sucursal, provincia, ciudad, direccion, descripcion, hora_de_trabajo, atencion_al_cliente)
-    VALUES '.implode(', ', $sucursales).';';
-
-echo $sql;
