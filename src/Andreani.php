faf54d20b29614bb261c885d12cd56e988426975
Alejo Sotelo <alejoasotelo@outlook.com>
2022-05-05 12:50:33 -0300

    v0.8.2

--- src/Andreani.php
+++ src/Andreani.php
@@ -18,7 +18,7 @@ class Andreani
     const ETIQUETA_ESTANDAR = '';
     const ETIQUETA_DOCUMENTO_DE_CAMBIO = 'documentoDeCambio';
 
-    private $version = '0.8.1';
+    private $version = '0.8.2';
 
     private $debug = true;
     private $http = null;
